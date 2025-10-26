17447eb90d31400619e45d13bab08a9923728da3
Guillem <guillemarana21@gmail.com>
2025-10-26 20:12:00 +0100

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DragonBall } from './pages/dragonball/dragonball';
+import { DragonballSuper } from './pages/dragonball-super/dragonball-super';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {
+    path: 'dragonball',
+    component: DragonBall,
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonballSuper,
+  },
+  {
+    path: '**',
+    redirectTo: 'dragonball',
+  },
+];
